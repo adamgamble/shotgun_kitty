ccfe7ddc32622b3339d17f8a574e460f2aa6ee1a
Adam Gamble <adamgamble@gmail.com>
2014-05-13 23:04:48 -0500

    Removed servo library

--- motor_controller.h
+++ motor_controller.h
@@ -1,8 +1,6 @@
 #ifndef motor_controller_h
 #define motor_controller_h
 
-#include "Servo.h"
-
 class MotorController {
   public:
     MotorController();
@@ -11,10 +9,6 @@ class MotorController {
 
   private:
     void setup_motors();
-    Servo motor_1;
-    Servo motor_2;
-    Servo motor_3;
-    Servo motor_4;
 };
 
 #endif
